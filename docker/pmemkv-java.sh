6257bdeec7b3cf59172a7bb5257bb10efe185f56
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-09-09 18:03:59 +0200

    docker: handle extra maven parameters to properly build pmemkv-java
    it's especially useful for building docker images behind proxies;
    Right now, it's the only parameter(s) handled in 'setup-maven-settings.sh'.

--- docker/pmemkv-java.sh
+++ docker/pmemkv-java.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -ex
 
+# Include setup of extra maven parameters (proxies)
+source /setup-maven-settings.sh
+
+MVN_PARAMS="${PMEMKV_MVN_PARAMS}"
+echo "Extra mvn params (taken from env variable): ${MVN_PARAMS}"
+
 git clone https://github.com/pmem/pmemkv-java.git
 pushd pmemkv-java
 
-mvn install -Dmaven.test.skip=true
+mvn install -Dmaven.test.skip=true ${MVN_PARAMS}
 
 popd
 rm -r pmemkv-java
